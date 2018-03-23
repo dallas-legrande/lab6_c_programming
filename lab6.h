94f5786d25732e65b9333b52aeb8c21b321bf1c7
dallas-legrande <33949897+dallas-legrande@users.noreply.github.com>
2018-03-23 10:00:13 -0600

    Create lab6.h
    header file for tying all the functions files to this program

---
+++ lab6.h
@@ -0,0 +1,13 @@
+/* author: Dallas LeGrande
+ * CSCI112 lab6
+ * March 20, 2018
+ * file to declare all the functions that are in separate files
+ */ 
+
+void strnum();
+void lower();
+void upper();
+void palin();
+void int_or_double();
+
+
