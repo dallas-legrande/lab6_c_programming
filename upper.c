60723ed7fb83ba7fa8b439cab2aea004362ec5ac
dallas-legrande <33949897+dallas-legrande@users.noreply.github.com>
2018-03-23 10:06:59 -0600

    Create upper.c
    checks the read in string to see how many uppercase letters it has if it has any

---
+++ upper.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdio.h>
+#include "lab6.h"
+
+/* Author: Dallas LeGrande
+ * CSCI112 lab6 method
+ * March 20, 2018
+ *
+ * This implements the check for upper case letters method by checking each character against the range of ascii upper case characters
+ */
+
+void upper(char *str)
+{
+    int i;
+    int counter = 0;// initializes the counter to count the upper case letters
+    for(i = 0; i < strlen(str); i++)// goes through the lengh of the string checking each character
+	{
+	    if(str[i] >= 65 && str[i] <= 90)// if the character is in this range in the ascii code then it is an uppercase letter
+		{
+		    counter++;// increase the counter everytime an uppercase letter is found
+		}
+	}// end of for loop
+    printf("It has %d uppercase letters.\n", counter);
+}
