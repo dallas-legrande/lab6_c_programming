117495d2fe23d77f0ca491239ba8fe619345075c
dallas-legrande <33949897+dallas-legrande@users.noreply.github.com>
2018-03-23 10:01:14 -0600

    Create lower.c
    checks the string to see if it has lower case numbers and counts how many there are if there are any

---
+++ lower.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdio.h>
+#include "lab6.h"
+
+/* Author: Dallas LeGrande
+ * CSCI112 lab6 method
+ * March 20, 2018
+ *
+ * This implements the check for lower case letters method by checking each character against the range of ascii lower case characters
+ */
+
+void lower(char *str)
+{
+    int i;
+    int counter = 0;// initializes the counter to count the lower case letters
+    for(i = 0; i < strlen(str); i++)// goes through the lengh of the string checking each character
+	{
+	    if(str[i] >= 97 && str[i] <= 122)// if the character is in this range in the ascii code then it is a lowercase letter
+		{
+		    counter++;// increase the counter everytime a lowercase letter is found
+		}
+	}// end of for loop
+    printf("It has %d lowercase letters.\n", counter);
+}
