8c245070c2a37f71b3c9265f9cf8d5ec33ee139f
dallas-legrande <33949897+dallas-legrande@users.noreply.github.com>
2018-03-26 16:33:34 -0600

    Update strnum.c

--- strnum.c
+++ strnum.c
@@ -11,27 +11,27 @@
 
 void strnum(char *str)
 {
-	typedef enum{false, true} bool;
-	bool match = 0;
-	int i;
-	// if the first spot in the string is a number or a point according to the ascii table then check the rest of the string to make sure all the characters are numbers
-	if(str[0] <= 57 && str[0] >= 48 || str[0] == 46) 
-	    {
-	       	match = 1; 
-		for(i = 1; i < strlen(str); i++)// check all the characters in the string to see if they are numbers or the point
-		    {
-	    		if(str[i] > 57 || str[i] < 48 && str[i] != 46)//if the number is less than 48 and is not 46 which is the period then it is not a number
-	    		    {
-				match = 0;
-			    }// end of inner if statement
-		    }//end of for loop
-	    }//end of outer if statement
-	if(match == 1)
-	    {
-		printf("String %s is a number.\n", str); //print that it is a number
-		int_or_double(str);
-	    }
-	else{
-		printf("String %s is not a number.\n", str); //print that it is not a number
-	    }
+        typedef enum{false, true} bool;
+        bool match = 0;
+        int i;
+        // if the first spot in the string is a number or a point according to the ascii table then check the rest of the string to make sure all the characters are numbers
+        if(str[0] <= 57 && str[0] >= 48 || str[0] == 46)
+            {
+                match = 1;
+                for(i = 1; i < strlen(str); i++)// check all the characters in the string to see if they are numbers or the point
+                    {
+                        if(str[i] > 57 || str[i] < 48 && str[i] != 46)//if the number is less than 48 and is not 46 which is the period then it is not a number
+                            {
+                                match = 0;
+                            }// end of inner if statement
+                    }//end of for loop
+            }//end of outer if statement
+        if(match == 1)
+            {
+                printf("String <%s> is a number.\n", str); //print that it is a number
+                int_or_double(str);
+            }
+        else{
+                printf("String <%s> is not a number.\n", str); //print that it is not a number
+            }
 }
