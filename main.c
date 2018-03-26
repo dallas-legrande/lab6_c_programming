52961f1105060cc90e27624c8da89cf6628d0293
dallas-legrande <33949897+dallas-legrande@users.noreply.github.com>
2018-03-26 12:09:42 -0600

    Update main.c
    main function reads in the first line and tokens it out. still need to figure out how to read the second line and do the same

--- main.c
+++ main.c
@@ -11,45 +11,39 @@
  * The questions to be answered for line two are: is this a string a number; if yes is it an integer or a real number
  */
 
+void test(char *word)
+{
+        int length = strlen(word);
+        palin(word);
+        printf("The first character is %c\n", word[0]);
+        printf("It is of length %d\n", length);
+        upper(word);
+        lower(word);
+        strnum(word);
+        printf("\n");
+}
+
 int main(int argc, char** argv)
 {
-	int j;
-	int counter = 0, i = 0, word_count = 0;
-	char a, c;
-	char temp_array [1000];
-	char str[100];
-	char *token = strtok(str, " ");
-	char *word = "100.001";
-	int length = strlen(word);
+        printf("\n"); //prints a blank line to differentiate where the program starts from the line commands
+        char str[100]; //variable to hold the string being read in
+        char copy[100]; //place to copy the read in string so that it can be manipulated
+        char* word;
 
-	palin(word);
-	printf("The first character is %c\n", word[0]);
-	printf("It is of length %d\n", length);
-	upper(word);
-	lower(word);
-	strnum(word);
+        scanf("%[^\n]s",&str); //%[^\n]s reads the line until it hits an \n
+        //printf("This is the string that is read in %s\n", str);
+        strcpy(copy, str); //copies the read in string to another variable
+        //printf("This is copy %s\n", copy);
+        word = strtok(copy, " "); //separates the string into words every time there is a space
+        test(word); //Tests the first word in the string
 
-/*	while(token != NULL)
-	    {
-		strncat(str,token, 20);
-		/*for(j = 0; j < sizeof(str); j++)
-		{		
-			temp_array[j] = 
-		}
-		i++;
-		token = strtok(str, " ");
-		*/
-		//scanf("%c", &c);
-		//temp_array[i] = c;
-		//counter++;
-		//i++;
-		//printf("This is what is in c %c\n", c);
-		//if(c == ' ')
-		//{
-		//	printf("C is now this %c\n", c);
-		//}
-	//	printf("This is what is in str %s\n", str);
-	
-	    //}
-	return (0);
+//      printf("This is the first word %s\n", word);
+        while(word != NULL) //continues going through the string until the word equals  NULL
+            {
+                word = strtok(NULL, " "); //tokens the string. Since the string being tokened is NULL is continues on from the original string passed in finding each successive word
+                if(word != NULL){ //once the token gets to the end it will set word to NULL. Do not want NULL to be tested so if statement added
+                    test(word); //tests each word as it is tokenized
+                }//end of if
+            }//end of while
+        return (0);
 }
