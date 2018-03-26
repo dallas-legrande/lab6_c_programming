b3d20b129af384fa8dbe3f646ff0fc81e939453b
dallas-legrande <33949897+dallas-legrande@users.noreply.github.com>
2018-03-26 16:33:09 -0600

    Update palin.c

--- palin.c
+++ palin.c
@@ -14,33 +14,33 @@
 
 void palin(char *str) //takes a string as an argumen and compares the letters to see if the string is a palindrome
 {
-	typedef enum {false,true} bool;
-	bool match = false;
-	int i = 0;
-	int length = strlen(str)-1; //gives the length of the string minus the space at the end
-	if(str[i] == str[length-i])// if the first and the last letter match then check the next ones
-	    {	
-		i++;
-		while(str[i] == str[length-i]) // while the next character and the next to last character checked match, then keep comparing the characters
-		    {
-			// if you make it to the character in the middle coming from both sides or the index counting from the end
-			// is becomes smaller than the index counting from the beginning, then you have a palindrome
-			if(i == (length-i) || length-i > i)
-			    {	
-				match = true; //set match to true because you found a palindrome
-			    }			
-			i++;
-		    }// end of while loop
-		if(match == 1)// if match it true
-		{
-		    printf("String %s is a palindrome.\n", str); // prints when the string is a palindrome
-		}
-		else{
-		    printf("String %s is not a palindrome.\n", str); // prints when the string is not a palindrome
-		}
-	    }// end of if statement
-	else //if the first and last letters don't match then you alread know it's not a palindrome and no further checking is needed
-	    {
-		printf("String %s is not a palindrome.\n", str); // prints when the string is not a palindrome
-   	    }
+        typedef enum {false,true} bool;
+        bool match = false;
+        int i = 0;
+        int length = strlen(str)-1; //gives the length of the string minus the space at the end
+        if(str[i] == str[length-i])// if the first and the last letter match then check the next ones
+            {
+                i++;
+                while(str[i] == str[length-i]) // while the next character and the next to last character checked match, then keep comparing the characters
+                    {
+                        // if you make it to the character in the middle coming from both sides or the index counting from the end
+                        // is becomes smaller than the index counting from the beginning, then you have a palindrome
+                        if(i == (length-i) || length-i > i)
+                            {
+                                match = true; //set match to true because you found a palindrome
+                            }
+                        i++;
+                    }// end of while loop
+                if(match == 1)// if match it true
+                {
+                    printf("String <%s> is a palindrome.\n", str); // prints when the string is a palindrome
+                }
+                else{
+                    printf("String <%s> is not a palindrome.\n", str); // prints when the string is not a palindrome
+                }
+            }// end of if statement
+        else //if the first and last letters don't match then you alread know it's not a palindrome and no further checking is needed
+            {
+                printf("String <%s> is not a palindrome.\n", str); // prints when the string is not a palindrome
+            }
 }
