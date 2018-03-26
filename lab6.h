da3f8bdfd24a401c03a5c43633a78bbff000b633
dallas-legrande <33949897+dallas-legrande@users.noreply.github.com>
2018-03-26 16:39:34 -0600

    Update lab6.h

--- lab6.h
+++ lab6.h
@@ -1,13 +1,13 @@
 /* author: Dallas LeGrande
  * CSCI112 lab6
  * March 20, 2018
- * file to declare all the functions that are in separate files
- */ 
+ * Header file to declare all the functions that are in separate files
+ */
 
-void strnum();
-void lower();
-void upper();
-void palin();
-void int_or_double();
+void strnum(); //method for checking if string or a number is being passed in
+void lower(); //method to check for lower case letters
+void upper(); //method to check for upper case letters
+void palin(); //method to check for palindromes
+void int_or_double(); //method to check if a number is an integer or a real number
 
 
