b8dd6ced70c6d4f0d2190c71666ef67def2e1053
dallas-legrande <33949897+dallas-legrande@users.noreply.github.com>
2018-03-26 16:33:57 -0600

    Update upper.c

--- upper.c
+++ upper.c
@@ -14,11 +14,11 @@ void upper(char *str)
     int i;
     int counter = 0;// initializes the counter to count the upper case letters
     for(i = 0; i < strlen(str); i++)// goes through the lengh of the string checking each character
-	{
-	    if(str[i] >= 65 && str[i] <= 90)// if the character is in this range in the ascii code then it is an uppercase letter
-		{
-		    counter++;// increase the counter everytime an uppercase letter is found
-		}
-	}// end of for loop
-    printf("It has %d uppercase letters.\n", counter);
+        {
+            if(str[i] >= 65 && str[i] <= 90)// if the character is in this range in the ascii code then it is an uppercase letter
+                {
+                    counter++;// increase the counter everytime an uppercase letter is found
+                }
+        }// end of for loop
+    printf("It has <%d> uppercase letters.\n", counter);
 }
