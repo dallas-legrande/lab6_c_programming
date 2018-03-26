086dc6a5462ae21828edebd40c13d35e0e702e16
dallas-legrande <33949897+dallas-legrande@users.noreply.github.com>
2018-03-26 16:32:02 -0600

    Update lower.c

--- lower.c
+++ lower.c
@@ -14,11 +14,11 @@ void lower(char *str)
     int i;
     int counter = 0;// initializes the counter to count the lower case letters
     for(i = 0; i < strlen(str); i++)// goes through the lengh of the string checking each character
-	{
-	    if(str[i] >= 97 && str[i] <= 122)// if the character is in this range in the ascii code then it is a lowercase letter
-		{
-		    counter++;// increase the counter everytime a lowercase letter is found
-		}
-	}// end of for loop
-    printf("It has %d lowercase letters.\n", counter);
+        {
+            if(str[i] >= 97 && str[i] <= 122)// if the character is in this range in the ascii code then it is a lowercase letter
+                {
+                    counter++;// increase the counter everytime a lowercase letter is found
+                }
+        }// end of for loop
+    printf("It has <%d> lowercase letters.\n", counter);
 }
